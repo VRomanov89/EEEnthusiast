c6eda60a9634e3177f3020e793af7a3c909013c6
Vladimir Romanov <vladromweb@gmail.com>
2017-05-04 13:16:59 -0700

    Advanced Input Control Arduino
    Advanced Input Control Arduino

---
+++ 03. Arduino Tutorials/01. Advanced Button Control/ButtonSketch/ButtonSketch.ino
@@ -0,0 +1,114 @@
+const int numOfInputs = 2;
+const int inputPins[numOfInputs] = {2,3};
+const int outputPin = 10;
+
+int LEDState = 0;
+int inputState[numOfInputs];
+int lastInputState[numOfInputs] = {LOW,LOW};
+bool inputFlags[numOfInputs] = {LOW,LOW};
+int inputCounters[numOfInputs];
+
+long lastDebounceTime[numOfInputs] = {0,0};
+long debounceDelay = 50;
+
+void setup() {
+  for(int i = 0; i < numOfInputs; i++) {
+    pinMode(inputPins[i], INPUT);
+    digitalWrite(inputPins[i], HIGH);
+  }
+  Serial.begin(9600);
+  pinMode(outputPin, OUTPUT);
+  //digitalWrite(outputPin, outputState);
+}
+
+void loop() {
+  setInputFlags();
+  resolveInputFlags();
+  resolveOutputs();
+}
+
+void setInputFlags() {
+  for(int i = 0; i < numOfInputs; i++) {
+    int reading = digitalRead(inputPins[i]);
+    if (reading != lastInputState[i]) {
+      lastDebounceTime[i] = millis();
+    }
+    if ((millis() - lastDebounceTime[i]) > debounceDelay) {
+      if (reading != inputState[i]) {
+        inputState[i] = reading;
+        if (inputState[i] == HIGH) {
+          //outputState = !outputState;
+          inputFlags[i] = HIGH;
+        }
+      }
+    }
+    lastInputState[i] = reading;
+  }
+}
+
+void resolveInputFlags() {
+  for(int i = 0; i < numOfInputs; i++) {
+    if(inputFlags[i] == HIGH) {
+      // Input Toggle Logic
+      inputCounters[i]++;
+      updateLEDState(i);
+
+      
+      printString(i);
+      inputFlags[i] = LOW;
+    }
+  }
+}
+
+void printString(int output) {
+      Serial.print("Input ");
+      Serial.print(output);
+      Serial.print(" was pressed ");
+      Serial.print(inputCounters[output]);
+      Serial.println(" times.");
+}
+
+void updateLEDState(int input) {
+  if(input == 0) {
+    if(LEDState == 0) {
+      LEDState = 1;
+    }else{
+      LEDState = 0;
+    }
+  }else if(input == 1) {
+    if(LEDState == 0 || LEDState == 1 || LEDState > 5) {
+      LEDState = 2;
+    }else{
+      LEDState++;
+    }
+  }
+}
+
+void resolveOutputs() {
+  switch (LEDState) {
+    case 0:
+      digitalWrite(outputPin, LOW);
+      break;
+    case 1:
+      digitalWrite(outputPin, HIGH);
+      break;
+    case 2:
+      analogWrite(outputPin, 30);
+      break;
+    case 3:
+      analogWrite(outputPin, 70);
+      break;
+    case 4:
+      analogWrite(outputPin, 100);
+      break;
+    case 5:
+      analogWrite(outputPin, 155);
+      break;
+    case 6:
+      analogWrite(outputPin, 255);
+      break;
+    default: 
+    break;
+  }
+}
+
